c5368da09ffecdb3ab188a1e5b0f649b26f012b4
tustin2121 <tustin2121@gmail.com>
2019-03-07 22:18:01 -0500

    Adding random seed setting to the debug menu

--- include/te_debug.h
+++ include/te_debug.h
@@ -6,10 +6,11 @@
 struct DebugInterrupts
 {
     u8 funcId;
-	u8 args[7];
+	s8 returnVal;
+	u8 args[12]; //args[2] is word aligned
     bool8 skipBattles : 1;
-    
-};
+    // 15 bits remaining
+}; //size = 0x10
 // Defined in main.c for memory location purposes
 extern struct DebugInterrupts gDebugInterrupts;
 
--- src/te_debug.c
+++ src/te_debug.c
@@ -3,6 +3,7 @@
 #include "start_menu.h"
 #include "pokemon_storage_system.h"
 #include "overworld.h"
+#include "random.h"
 #include "constants/maps.h"
 
 // Reference to an assembly defined constant, the start of the ROM
@@ -10,33 +11,46 @@
 extern const int Start;
 extern void nullsub_129(void);
 
+typedef void (*DebugFunc)(void);
+
 static void DebugHandleEmergencySave();
 static void DebugHandleShowPCBox();
 static void DebugHandleWarpRequest();
 static void DebugHandleReloadMap();
+static void DebugHandleGetRandomSeeds();
+static void DebugHandleSetRandomSeeds();
 
-#define SET_SUCCESS() gDebugInterrupts.args[0] = 1
-#define SET_FAILURE() gDebugInterrupts.args[0] = -1
+#define SET_SUCCESS() gDebugInterrupts.returnVal = 1
+#define SET_FAILURE() gDebugInterrupts.returnVal = -1
 
-static void (*const sDebugCommands[])(void) =
+static const DebugFunc sDebugCommands[] =
 {
 	nullsub_129,
 	DebugHandleEmergencySave,
 	DebugHandleShowPCBox,
 	DebugHandleWarpRequest,
 	DebugHandleReloadMap,
+	DebugHandleGetRandomSeeds,
+	DebugHandleSetRandomSeeds,
 };
+
+#define DEBUGFN_COUNT ((int)(sizeof(sDebugCommands)/sizeof(DebugFunc)))
 
 void DebugCheckInterrupts()
 {
 	if (gDebugInterrupts.funcId != 0)
 	{
-		sDebugCommands[gDebugInterrupts.funcId]();
+		gDebugInterrupts.returnVal = 0;
+		if (gDebugInterrupts.funcId < DEBUGFN_COUNT)
+		{
+			sDebugCommands[gDebugInterrupts.funcId]();
+		}
 		gDebugInterrupts.funcId = 0;
 	}
 }
 
 // parameters: none
+// returns: none
 void DebugHandleEmergencySave()
 {
 	SaveGame(); //"Would you like to save the game?"
@@ -44,6 +58,7 @@ void DebugHandleEmergencySave()
 }
 
 // parameters: none
+// returns: none
 void DebugHandleShowPCBox()
 {
 	ShowPokemonStorageSystemPC();
@@ -56,6 +71,7 @@ void DebugHandleShowPCBox()
 //	 args[2] = warpId
 //	 args[3] = x
 //	 args[4] = y
+// returns: none
 void DebugHandleWarpRequest()
 {
 	s8 args[5];
@@ -82,11 +98,36 @@ error:
 	return;
 }
 
+// arguments: none
 void DebugHandleReloadMap()
 {
 	struct WarpData data = gSaveBlock1Ptr->location;
 	
 	SetWarpDestination(data.mapGroup, data.mapNum, -1, data.x, data.y);
 	WarpIntoMap();
+	SET_SUCCESS();
+}
+
+// arguments: none
+// returns: gRngValue copied to +4, gRng2Value copied to +8
+void DebugHandleGetRandomSeeds()
+{
+	u32 *val1 = &gDebugInterrupts.args[2]; //choose word-aligned address
+	u32 *val2 = &gDebugInterrupts.args[6];
+	*val1 = gRngValue;
+	*val2 = gRng2Value;
+	SET_SUCCESS();
+}
+
+// arguments: 
+// 	 args[2]+4 = gRngValue to copy
+//   args[6]+4 = gRng2Value to copy
+// returns: none
+void DebugHandleSetRandomSeeds()
+{
+	u32 *val1 = &gDebugInterrupts.args[2]; //choose word-aligned address
+	u32 *val2 = &gDebugInterrupts.args[6];
+	gRngValue = *val1;
+	gRng2Value = *val2;
 	SET_SUCCESS();
 }
