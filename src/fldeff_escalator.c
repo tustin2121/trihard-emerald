e8c759b69c28612e79572a13c46c89d86cb05177
Tustin2121 <tustin2121@gmail.com>
2019-04-19 18:12:27 -0400

    Second Floor Centers are now sleeping centers
    - Initial pass at the trainer lounger and sleeping quarters of the Pokemon Centers.
    - The second floors need a lot more warps and/or signposts to allow/precent access to sleeping rooms. It might also need more sleeping rooms.

--- src/fldeff_escalator.c
+++ src/fldeff_escalator.c
@@ -7,7 +7,7 @@
 
 static EWRAM_DATA u8 sEscalatorAnim_TaskId = 0;
 
-static void sub_80E12E8(u8 taskId, const s16 *list, u16 isImpassableFlag)
+static void EscalatorChangeTiles(u8 taskId, const s16 *list, u16 isImpassableFlag)
 {
     s16 r5 = gTasks[taskId].data[4] - 1;
     s16 r3 = gTasks[taskId].data[5] - 1;
@@ -53,15 +53,16 @@ static void sub_80E12E8(u8 taskId, const s16 *list, u16 isImpassableFlag)
     }
 }
 
-static const u16 gUnknown_08589ABA[] = {0x284, 0x282, 0x280};
-static const u16 gUnknown_08589AC0[] = {0x285, 0x283, 0x281};
-static const u16 gUnknown_08589AC6[] = {0x28C, 0x28A, 0x288};
-static const u16 gUnknown_08589ACC[] = {0x28D, 0x28B, 0x289};
-static const u16 gUnknown_08589AD2[] = {0x2A0, 0x2A2, 0x2A4};
-static const u16 gUnknown_08589AD8[] = {0x2A1, 0x2A3, 0x2A5};
-static const u16 gUnknown_08589ADE[] = {0x2A8, 0x2AA, 0x2AC};
+static const u16 sEscalatorMetatiles_UpA[] = {0x284, 0x282, 0x280};
+static const u16 sEscalatorMetatiles_UpB[] = {0x285, 0x283, 0x281};
+static const u16 sEscalatorMetatiles_UpC[] = {0x28C, 0x28A, 0x288};
+static const u16 sEscalatorMetatiles_UpD[] = {0x28D, 0x28B, 0x289};
+static const u16 sEscalatorMetatiles_DownA[] = {0x2A0, 0x2A2, 0x2A4};
+static const u16 sEscalatorMetatiles_DownB[] = {0x2A1, 0x2A3, 0x2A5};
+static const u16 sEscalatorMetatiles_DownC[] = {0x2A8, 0x2AA, 0x2AC};
+static const u16 sEscalatorMetatiles_DownPlant[] = {0x308, 0x309, 0x30A};
 
-static void sub_80E1444(u8 taskId)
+static void Task_EscalatorAnimateTiles(u8 taskId)
 {
     s16 *data = gTasks[taskId].data;
 
@@ -70,25 +71,28 @@ static void sub_80E1444(u8 taskId)
     switch (data[0])
     {
         case 0:
-            sub_80E12E8(taskId, gUnknown_08589ABA, 0);
+            EscalatorChangeTiles(taskId, sEscalatorMetatiles_UpA, 0);
             break;
         case 1:
-            sub_80E12E8(taskId, gUnknown_08589AC0, 0);
+            EscalatorChangeTiles(taskId, sEscalatorMetatiles_UpB, 0);
             break;
         case 2:
-            sub_80E12E8(taskId, gUnknown_08589AC6, METATILE_COLLISION_MASK);
+            EscalatorChangeTiles(taskId, sEscalatorMetatiles_UpC, METATILE_COLLISION_MASK);
             break;
         case 3:
-            sub_80E12E8(taskId, gUnknown_08589ACC, 0);
+            EscalatorChangeTiles(taskId, sEscalatorMetatiles_UpD, 0);
             break;
         case 4:
-            sub_80E12E8(taskId, gUnknown_08589AD2, METATILE_COLLISION_MASK);
+            EscalatorChangeTiles(taskId, sEscalatorMetatiles_DownA, METATILE_COLLISION_MASK);
             break;
         case 5:
-            sub_80E12E8(taskId, gUnknown_08589AD8, 0);
+            EscalatorChangeTiles(taskId, sEscalatorMetatiles_DownB, 0);
             break;
         case 6:
-            sub_80E12E8(taskId, gUnknown_08589ADE, 0);
+            EscalatorChangeTiles(taskId, sEscalatorMetatiles_DownC, 0);
+            break;
+        case 7:
+            EscalatorChangeTiles(taskId, sEscalatorMetatiles_DownPlant, 0);
             break;
     }
 
@@ -101,22 +105,22 @@ static void sub_80E1444(u8 taskId)
     }
 }
 
-static u8 sub_80E150C(u16 var)
+static u8 CreateEscaltorAnimationTask(u16 var)
 {
-    u8 taskId = CreateTask(sub_80E1444, 0);
+    u8 taskId = CreateTask(Task_EscalatorAnimateTiles, 0);
     s16 *data = gTasks[taskId].data;
 
     PlayerGetDestCoords(&data[4], &data[5]);
     data[0] = 0;
     data[1] = 0;
     data[2] = var;
-    sub_80E1444(taskId);
+    Task_EscalatorAnimateTiles(taskId);
     return taskId;
 }
 
 void sub_80E1558(u8 var)
 {
-    sEscalatorAnim_TaskId = sub_80E150C(var);
+    sEscalatorAnim_TaskId = CreateEscaltorAnimationTask(var);
 }
 
 void sub_80E1570(void)
