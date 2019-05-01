33c8da208520dbb6e60c156a80c0dc4573169487
Marcus Huderle <huderlem@gmail.com>
2019-05-01 16:35:31 -0500

    Add a bunch more PC scripts

--- src/field_specials.c
+++ src/field_specials.c
@@ -4206,3 +4206,35 @@ u8 sub_813BF7C(void)
 {
     return sub_813BADC(gSpecialVar_0x8004);
 }
+
+static void PCBangShakingEffect(u8 taskId)
+{
+    struct Task *task = &gTasks[taskId];
+
+    if (++task->data[1] % task->data[5] == 0)
+    {
+        task->data[1] = 0;
+        task->data[2]++;
+        task->data[4] = -task->data[4];
+        if (task->data[4] > 0)
+            task->data[4]--;
+
+        SetCameraPanning(0, task->data[4]);
+        if (task->data[4] == 0)
+        {
+            DestroyTask(taskId);
+            EnableBothScriptContexts();
+            InstallCameraPanAheadCallback();
+        }
+    }
+}
+
+void DoPCBangShakingEffect(void)
+{
+    u8 taskId = CreateTask(PCBangShakingEffect, 3);
+    gTasks[taskId].data[1] = 0;
+    gTasks[taskId].data[2] = 0;
+    gTasks[taskId].data[4] = 6;
+    gTasks[taskId].data[5] = 3;
+    SetCameraPanningCallback(0);
+}
