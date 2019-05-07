5d718022bf32fa13ec9c125ea36e2d668f0e79d2
Marcus Huderle <huderlem@gmail.com>
2019-05-06 20:02:10 -0500

    Add more elegant dialog when buying TMs

--- include/strings.h
+++ include/strings.h
@@ -986,6 +986,7 @@ extern const u8 gText_Var1IsItThatllBeVar2[];
 extern const u8 gText_YouWantedVar1ThatllBeVar2[];
 extern const u8 gText_InBagVar1[];
 extern const u8 gText_Var1AndYouWantedVar2[];
+extern const u8 gText_YouWantTM[];
 extern const u8 gText_HereYouGoThankYou[];
 extern const u8 gText_NoMoreRoomForThis[];
 extern const u8 gText_ThankYouIllSendItHome[];
