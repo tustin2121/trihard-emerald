d958cf6006fd497fdc757e02f5765f1995315f79
Ericchi <>
2019-08-10 12:25:12 +0200

    More markasdialog

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -598,7 +598,6 @@
 #define MAP_ROUTE124_DIVING_TREASURE_HUNTERS_HOUSE (0 | (33 << 8))
 #define MAP_SOOTOPOLIS_LEGENDS_EDIT                (1 | (33 << 8))
 #define MAP_SCRIPT_TEST_MAP                        (2 | (33 << 8))
-#define MAP_MIRAGE_TOWER_LAYOUT                    (3 | (33 << 8))
 
 #define MAP_GROUPS_COUNT 34
 
